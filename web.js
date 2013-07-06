88e989478dd45557efcebb993b0ac0265bb0a9b1
Alejandro Barrientos <abarrien@qti.qualcomm.com>
2013-07-06 11:43:58 -0700

    reading man page from file instead of outputting string

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Startup Engineering Class - HW3!');
+  fileBuffer = fs.readFileSync('index.html');
+  response.send(fileBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
