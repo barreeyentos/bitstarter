5de735f72390feceb49fe02fa9ea347fde33f7bc
Alejandro Barrientos <abarrien@qti.qualcomm.com>
2013-07-06 17:00:40 -0700

    adding restler to check remote urls, adding restler to dependencies

--- grade.js
+++ grade.js
@@ -24,7 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var sys = require('util');
+var rest = require('restler');
+
+
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -35,6 +38,24 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertURLExists = function(url) {
+     var urlStr = url.toString();
+     rest.get(urlStr).on('complete', function(data, response) {
+	if(response.statusCode == 200) {
+		checkJson = checkHtml(data, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+    		console.log(outJson);
+	}
+	else {
+		console.log("%s doesn't exist.", urlStr);
+		process.exit(1); 
+	}
+       });
+
+};
+
+   
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,6 +76,18 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtml = function(html, checksfile) {
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+  
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +97,23 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_path>', 'Path to remote html file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if(program.file) {
+	checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+    }
+    else if(program.url) {
+	assertURLExists(program.url);
+    }
+    else {
+	console.log("You must supply either --file <html_file> or --url <url_path> to command line inputs");
+	process.exit(1);	
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
