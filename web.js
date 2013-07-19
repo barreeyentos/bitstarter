191963daf36e89834af655dab33768d436bdf8ba
Alejandro Barrientos <abarrien@qti.qualcomm.com>
2013-07-18 23:25:34 -0700

    changed node server to listen to port 8080 and serves static content in public, moved css out of index.html into its own file and service both files under public

--- web.js
+++ web.js
@@ -2,12 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  fileBuffer = fs.readFileSync('index.html');
-  response.send(fileBuffer.toString());
-});
+app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
